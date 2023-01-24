cb8800d2630819f8d717d443187ef6014b4c433a
jack morrison <bobimnot123@gmail.com>
2023-01-24 17:39:42 +0000

    removed comments and changed handler function to repeat twice

--- pico-pwm-audio.c
+++ pico-pwm-audio.c
@@ -7,12 +7,6 @@
  
 // Audio PIN is to match some of the design guide shields. 
 #define AUDIO_PIN 28  // you can change this to whatever you like
-
-/* 
- * This include brings in static arrays which contain audio samples. 
- * if you want to know how to make these please see the python code
- * for converting audio samples into static arrays. 
- */
 #include "Csine.h"
 int FreqCount = 2;
 int wav_position = 0;
@@ -25,20 +19,13 @@ float clkDiv=2.0f;
 float clockDivChnage( float curFrequency){
     return (curFrequency/frequency)*2.0f;
 }
-/*
- * PWM Interrupt Handler which outputs PWM level and advances the 
- * current sample. 
- * 
- * We repeat the same value for 8 cycles this means sample rate etc
- * adjust by factor of 8   (this is what bitshifting <<3 is doing)
- * 
- */
+
 void pwm_interrupt_handler() {
     pwm_clear_irq(pwm_gpio_to_slice_num(AUDIO_PIN));    
-    if (wav_position < (WAV_DATA_LENGTH<<3) - 1) { 
+    if (wav_position < round(WAV_DATA_LENGTH*clkDiv) - 1) { 
         // set pwm level 
         // allow the pwm value to repeat for 8 cycles this is >>3 
-        pwm_set_gpio_level(AUDIO_PIN, WAV_DATA[wav_position>>3]);  
+        pwm_set_gpio_level(AUDIO_PIN, WAV_DATA[round(wav_position/clkDiv)]);  
         wav_position++;
     } else {
         // reset to start
@@ -78,17 +65,6 @@ int main(void) {
 
     // Setup PWM for audio output
     pwm_config config = pwm_get_default_config();
-    /* Base clock 176,000,000 Hz divide by wrap 250 then the clock divider further divides
-     * to set the interrupt rate. 
-     * 
-     * 11 KHz is fine for speech. Phone lines generally sample at 8 KHz
-     * 
-     * 
-     * So clkdiv should be as follows for given sample rate
-     *  8.0f for 11 KHz
-     *  4.0f for 22 KHz
-     *  2.0f for 44 KHz etc
-     */
 
     pwm_config_set_clkdiv(&config, clkDiv); 
     pwm_config_set_wrap(&config, 250); 
