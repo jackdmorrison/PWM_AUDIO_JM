eee4823c12b91ae2e41c78f86cdf3dcf05ed1e1a
jack morrison <bobimnot123@gmail.com>
2023-02-23 16:11:57 +0000

    sdfghj

--- solution2.py
+++ solution2.py
@@ -5,7 +5,7 @@ tan_theta=1/86
 n_tan_theta = wavelength-pulseLength
 while wav_position < wavelength:
     if (wav_position <= (pulseLength) ) : 
-        print(255*((wav_position)*tan_theta))
+        print(255*(((wav_position)/86)))
         
 
     elif(wav_position<(wavelength) - 1):
