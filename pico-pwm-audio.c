e366b2bf382ae575dbc71a57cc1a2497435263f5
jack morrison <bobimnot123@gmail.com>
2023-04-10 08:28:20 +0100

    first attempt

--- pico-pwm-audio.c
+++ pico-pwm-audio.c
@@ -39,6 +39,7 @@ uint audio_pin_channel;
 uint audio_pin_channel2;
 
 void rawHandler1();
+void rawHandler2();
 int wav_position = 0;
 int wav_position2=0;
 float adc_value=0;
@@ -700,16 +701,49 @@ void pwm_interrupt_handler() {
 
     
 }
+void main2(){
+    adc_gpio_init(ADC_PIN2);
+    adc_select_input(1);
 
+    gpio_init(GATE2);
+    gpio_set_dir(GATE2,GPIO_IN);
+    gpio_pull_down(GATE2);
+    gpio_set_irq_enabled(GATE2,GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL,true);
+    gpio_add_raw_irq_handler_masked(( 0x01 << GATE2),&rawHandler2);
+
+    gpio_set_function(AUDIO_PIN2, GPIO_FUNC_PWM);
+
+    audio_pin_slice2 = pwm_gpio_to_slice_num(AUDIO_PIN2);
+    audio_pin_channel2 = pwm_gpio_to_channel(AUDIO_PIN2);
+    // Setup PWM interrupt to fire when PWM cycle is complete
+    pwm_clear_irq(audio_pin_slice2);
+    pwm_set_irq_enabled(audio_pin_slice2, true);
+    // set the handle function above
+    irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_interrupt_handler); 
+    irq_set_enabled(PWM_IRQ_WRAP, true);
+ 
+    // Setup PWM for audio output
+    config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, clkDiv); 
+    pwm_config_set_wrap(&config, wrap); 
+    //pwm_config_set_phase_correct(&config,true);
+    pwm_init(audio_pin_slice2, &config, true);
+
+    pwm_set_gpio_level(AUDIO_PIN2, 0);
+    while(1) {
+        __wfi(); // Wait for Interrupt
+    }
+}
 int main(void) {
     /* Overclocking for fun but then also so the system clock is a 
      * multiple of typical audio sampling rates.
      */
     stdio_init_all();
+    multicore_launch_core1(main2);
+
     adc_init();
     adc_gpio_init(ADC_PIN);
-    adc_gpio_init(ADC_PIN2);
-    adc_select_input(1);
+    
 
 
     gpio_init(GATE);
@@ -718,11 +752,7 @@ int main(void) {
     gpio_set_irq_enabled(GATE,GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL,true);
     gpio_add_raw_irq_handler_masked(( 0x01 << GATE),&rawHandler1);
 
-    gpio_init(GATE2);
-    gpio_set_dir(GATE2,GPIO_IN);
-    gpio_pull_down(GATE2);
-    gpio_set_irq_enabled(GATE2,GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL,true);
-    gpio_add_raw_irq_handler_masked(( 0x01 << GATE2),&rawHandler1);
+    
 
     button_t *sine = create_button(SINE, onchange);
     button_t *square = create_button(SQUARE, onchange);
@@ -739,7 +769,7 @@ int main(void) {
     vreg_set_voltage(VREG_VOLTAGE_1_05);
     set_sys_clock_khz(clockFreq, true); 
     gpio_set_function(AUDIO_PIN, GPIO_FUNC_PWM);
-    gpio_set_function(AUDIO_PIN2, GPIO_FUNC_PWM);
+    
 
     audio_pin_slice = pwm_gpio_to_slice_num(AUDIO_PIN);
     audio_pin_channel = pwm_gpio_to_channel(AUDIO_PIN);
@@ -815,6 +845,39 @@ void rawHandler1(){
         PLAY=false;
     }
     else if(gpio_get_irq_event_mask(GATE2) & GPIO_IRQ_EDGE_RISE){
+        adc_select_input(2);
+        gpio_acknowledge_irq(GATE2, GPIO_IRQ_EDGE_RISE );
+        adc_value=((adc_read())*conversionfactor);
+        subScript2=round(60*adc_value/3);
+        if(subScript2>60){
+            frequency2=freqList[0];
+        }else if(subScript2<0){
+            frequency2=freqList[60];
+        }else{
+            frequency2=freqList[subScript2];
+        }
+        currentF2 = freqList[subScript2];
+        if(subScript2==60){
+            upperVibrato2=highestFrequency;
+            lowerVibrato2=freqList[subScript2-1];
+        }else if(subScript2==0){
+            upperVibrato2=freqList[subScript2+1];
+            lowerVibrato2=lowestFrequency;
+        }else{
+            upperVibrato2=freqList[subScript2+1];
+            lowerVibrato2=freqList[subScript2-1];
+        }
+        vibchangeParam2 = (upperVibrato2-lowerVibrato2)/24;
+        
+        updateClockDiv(clockDivChange(frequency2),AUDIO_PIN2,audio_pin_slice2);
+        PLAY2=true;
+    }else if(gpio_get_irq_event_mask(GATE2) & GPIO_IRQ_EDGE_FALL){
+        gpio_acknowledge_irq(GATE2, GPIO_IRQ_EDGE_FALL );
+        PLAY2=false;
+    }
+}
+void rawHandler2(){
+    if(gpio_get_irq_event_mask(GATE2) & GPIO_IRQ_EDGE_RISE){
         adc_select_input(2);
         gpio_acknowledge_irq(GATE2, GPIO_IRQ_EDGE_RISE );
         adc_value=((adc_read())*conversionfactor);
